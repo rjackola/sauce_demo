a91fe51fb625f550abe00aac4dcd8492b5f5d51d
Ryan Jackola <ryanjduo@gmail.com>
2021-05-10 20:27:47 -0400

    Update 3

--- src/__Test__/saucePage.test.ts
+++ src/__Test__/saucePage.test.ts
@@ -1,34 +1,41 @@
 import { Builder, Capabilities } from "selenium-webdriver"
 const chromedriver = require('chromedriver')
 import {saucePage} from '../pageObjects/saucePage'
+import {BasePage} from '../pageObjects/basePage'
 
 const myDriver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 const myPage = new saucePage (myDriver, 'https://www.saucedemo.com/')
 
-
-
-describe("Sauce test suite", async ()=>
-it('Opens Sauce Demo website', async () => {
-await myPage.navigate()
-await myPage.setInput(myPage.username, 'standard_user')
-await myPage.setInput(myPage.password, 'secret_sauce')
-await myPage.click(myPage.loginButton)
-})
-)
-it('adds items to cart', async() =>{
+    test('Opens Sauce Demo website & logs in', async () => {
     await myPage.navigate()
     await myPage.setInput(myPage.username, 'standard_user')
     await myPage.setInput(myPage.password, 'secret_sauce')
     await myPage.click(myPage.loginButton)
-    await myPage.click(myPage.addBackpack)
-    await myPage.click(myPage.cartButton)
-} )
-   
-    
-    afterAll (async () => {
-        await myDriver.quit ()
-        console.log ("Test was succesful!")
+})
+
+    test('Adds item to cart', async() =>{
+        await myPage.navigate()
+        await myPage.setInput(myPage.username, 'standard_user')
+        await myPage.setInput(myPage.password, 'secret_sauce')
+        await myPage.click(myPage.loginButton)
+        await myPage.click(myPage.addBackpack)
+    } )
+
+    test('Goes to Cart Page', async() =>{
+        await myPage.click(myPage.cartButton)
     })
 
-   
+    test('Can Remove from Cart', async() =>{
+        await myPage.click(myPage.removeBackpack)
+
+})
+    test('Go to Checkout Page', async() =>{
+    await myPage.click(myPage.continueShopping)
+    await myPage.click(myPage.addBackpack)
+    await myPage.click(myPage.cartButton)
+    await myPage.click(myPage.checkout)
+})
+afterAll(async () => {
+    await myDriver.quit();
+  });
