c708df2e0cb5bcf6e3213ba47472195f70a1aa27
briva002 <79668462+briva002@users.noreply.github.com>
2021-05-10 17:34:37 -0700

    Belindas update

--- src/__Test__/saucePage.test.ts
+++ src/__Test__/saucePage.test.ts
@@ -1,18 +1,40 @@
 import { Builder, Capabilities } from "selenium-webdriver"
 const chromedriver = require('chromedriver')
-import {saucePage} from ''
+import { saucePage } from '../pageObjects/saucePage'
 
 const myDriver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
-const myPage = new myPage (myDriver, 'https://www.saucedemo.com/')
+const myPage = new saucePage (myDriver, 'https://www.saucedemo.com/')
+
 
 test('Opens Sauce Demo website', async () => {
     await myPage.navigate()
-    await myPage.username()
-    await myPage.setInput('standard_user')
-    await myPage. setInput('secret_sauce')
-
-
-
-    //await myPage.driver.quit()
+    await myPage.setInput(myPage.username, 'standard_user')
+    await myPage.setInput(myPage.password, 'secret_sauce')
+    await myPage.click(myPage.loginButton)
 })
+
+  test('adds items to cart', async () => {
+    await myPage.navigate()
+    await myPage.setInput(myPage.username, 'standard_user')
+    await myPage.setInput(myPage.password, 'secret_sauce')
+    await myPage.click(myPage.loginButton)
+    await myPage.click(myPage.addBackpack)
+    await myPage.click(myPage.cartButton)
+})
+test('Goes to Cart Page', async() =>{
+  await myPage.click(myPage.cartButton)
+})
+
+test('Can Remove from Cart', async() =>{
+  await myPage.click(myPage.removeBackpack)
+})
+test('Go to Checkout Page', async() =>{
+await myPage.click(myPage.continueShopping)
+await myPage.click(myPage.addBackpack)
+await myPage.click(myPage.cartButton)
+await myPage.click(myPage.checkout)
+})
+afterAll(async () => {
+await myDriver.quit();
+});
